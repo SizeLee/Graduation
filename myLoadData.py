25228a3c96563ae4d59089d3ede9c902b58ecca8
SizeLee <nothingforname@gmail.com>
2017-05-09 22:14:42 +0800

    improve the load data module

--- myLoadData.py
+++ myLoadData.py
@@ -18,7 +18,7 @@ class loadIris:
 
     dataName = 'Iris'
 
-    def __init__(self, lossRate = 0, setlossValue = 0):
+    def __init__(self, lossRate = 0., setlossValue = 0.):
         fp = open('..\\iris.txt','r') #todo change the way open file
 
         self.DataX = None
@@ -120,5 +120,128 @@ class loadIris:
         self.DataTestX = self.DataTestXLoss
 
 
+class loadData:
 
-# loadiris = loadIris()
+    def __init__(self, filedirectory, lossRate = 0., setlossValue = 0.):
+        fp = open(filedirectory,'r') #todo change the way open file
+
+        self.DataX = None
+        self.DataTrainX = None
+        self.DataTrainXLoss = None
+        self.DataValX = None
+        self.DataValXLoss = None
+        self.DataTestX = None
+        self.DataTestXLoss = None
+
+        self.DataY = None
+        self.DataTrainY = None
+        self.DataTestY = None
+        self.DataValY = None
+
+        self.sampleList = None
+        self.sampleX = None
+        self.sampleY = None
+
+        self.lossRate = lossRate
+        self.setLossValue = setlossValue
+
+        sampleCount = 0
+        self.sampleList = []
+
+        yClassDic = dict()
+        yClassNum = 0
+        for line in fp:
+            line = line.rstrip('\n')
+            dataStrList = line.split(',')
+            if dataStrList[-1] not in yClassDic:
+                yClassDic[dataStrList[-1]] = yClassNum
+                yClassNum = yClassNum + 1
+
+        # print(yClassDic)
+        # print(len(yClassDic))
+        ylabelVecTemplate = []
+        for i in range(len(yClassDic)):
+            ylabelVecTemplate.append(0)
+
+        # print(ylabelVecTemplate)
+
+        fp.seek(0, 0)
+        for line in fp:
+            sampleCount += 1
+            line = line.rstrip('\n')
+            dataStrList = line.split(',')
+            linetemp = []
+            for data in dataStrList:
+                if data in yClassDic:
+                    ylabelVec = ylabelVecTemplate.copy()
+                    ylabelVec[yClassDic[data]] = 1
+                    # print(ylabelVec)
+                    linetemp.append(ylabelVec)
+                elif linetemp == []:
+                    linetemp.append([float(data)])
+                else:
+                    linetemp[0].append(float(data))
+            # print(linetemp)
+            self.sampleList.append(linetemp)
+
+        # print(self.sampleList)
+        # print(sampleCount)
+        ##load into memory
+        orderList = range(sampleCount)
+        randomChange = random.sample(orderList,sampleCount)
+        # print(randomChange)
+        # randomChange.sort()
+        # print(randomChange)
+        sampleTemp = self.sampleList.copy()
+        for i in orderList:
+            self.sampleList[i] = sampleTemp[randomChange[i]]
+
+        # print(sampleList)
+
+        self.sampleX = []
+        self.sampleY = []
+
+        for line in self.sampleList:
+            self.sampleX.append(line[0])
+            self.sampleY.append(line[1])
+
+        self.DataX = np.array(self.sampleX)
+        self.DataY = np.array(self.sampleY,dtype=float)
+
+        # print(self.IrisDataX)
+        # print(self.IrisDataY)
+
+        self.DataTrainX = self.DataX[:int(0.6 * sampleCount), :]
+        self.DataTrainY = self.DataY[:int(0.6 * sampleCount), :]
+
+        self.DataValX = self.DataX[int(0.6 * sampleCount):int(0.8 * sampleCount), :]
+        self.DataValY = self.DataY[int(0.6 * sampleCount):int(0.8 * sampleCount), :]
+
+        self.DataTestX = self.DataX[int(0.8 * sampleCount):, :]
+        self.DataTestY = self.DataY[int(0.8 * sampleCount):, :]
+
+        fp.close()
+
+        # print(self.DataTrainX.shape)
+        # print(self.DataTrainY.shape)
+        #
+        # print(self.DataValX.shape)
+        # print(self.DataValY.shape)
+        #
+        # print(self.DataTestX.shape)
+        # print(self.DataTestY.shape)
+
+        self.lossSimulator = dataLossSimulator.dataLossSimulator(self.DataX.shape[1], self.lossRate, self.setLossValue)
+
+        self.DataTrainXLoss = self.lossSimulator.lossSimulate(self.DataTrainX)
+        self.DataValXLoss = self.lossSimulator.lossSimulate(self.DataValX)
+        self.DataTestXLoss = self.lossSimulator.lossSimulate(self.DataTestX)
+
+        self.DataTrainX = self.DataTrainXLoss
+        self.DataValX = self.DataValXLoss
+        self.DataTestX = self.DataTestXLoss
+
+if __name__ == '__main__':
+    # loadiris = loadIris()
+    myloadData = loadData('iris.txt', 0.3, -1.)
+    print(myloadData.DataTrainX, '\n', myloadData.DataTrainY)
---
+++ testQt.py
@@ -0,0 +1,696 @@
+# import sys
+# from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication
+# from PyQt5.QtGui import QIcon
+#
+#
+# class Example(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#         textEdit = QTextEdit()
+#         self.setCentralWidget(textEdit)
+#
+#         exitAction = QAction(QIcon('exit24.png'), 'Exit', self)
+#         exitAction.setShortcut('Ctrl+Q')
+#         exitAction.setStatusTip('Exit application')
+#         exitAction.triggered.connect(self.close)
+#
+#         self.statusBar()
+#
+#         menubar = self.menuBar()
+#         fileMenu = menubar.addMenu('&File')
+#         fileMenu.addAction(exitAction)
+#
+#         toolbar = self.addToolBar('Exit')
+#         toolbar.addAction(exitAction)
+#
+#         self.setGeometry(300, 300, 350, 250)
+#         self.setWindowTitle('Main window')
+#         self.show()
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtWidgets import (QWidget, QPushButton,
+#                              QHBoxLayout, QVBoxLayout, QApplication)
+#
+#
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#         okButton = QPushButton("OK")
+#         cancelButton = QPushButton("Cancel")
+#
+#         hbox = QHBoxLayout()
+#         hbox.addStretch(1)
+#         hbox.addWidget(okButton)
+#         hbox.addWidget(cancelButton)
+#
+#         vbox = QVBoxLayout()
+#         vbox.addStretch(1)
+#         vbox.addLayout(hbox)
+#
+#         self.setLayout(vbox)
+#
+#         self.setGeometry(300, 300, 300, 150)
+#         self.setWindowTitle('Buttons')
+#         self.show()
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtWidgets import (QWidget, QGridLayout,
+#                              QPushButton, QApplication)
+#
+#
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#
+#         grid = QGridLayout()
+#         self.setLayout(grid)
+#
+#         names = ['Cls', 'Bck', '', 'Close',
+#                  '7', '8', '9', '/',
+#                  '4', '5', '6', '*',
+#                  '1', '2', '3', '-',
+#                  '0', '.', '=', '+']
+#
+#         positions = [(i, j) for i in range(5) for j in range(4)]
+#
+#         for position, name in zip(positions, names):
+#
+#             if name == '':
+#                 continue
+#             button = QPushButton(name)
+#             grid.addWidget(button, *position)
+#
+#         self.move(300, 150)
+#         self.setWindowTitle('Calculator')
+#         self.show()
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit,
+#                              QTextEdit, QGridLayout, QApplication)
+#
+#
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#         title = QLabel('Title')
+#         author = QLabel('Author')
+#         review = QLabel('Review')
+#
+#         titleEdit = QLineEdit()
+#         authorEdit = QLineEdit()
+#         reviewEdit = QTextEdit()
+#
+#         grid = QGridLayout()
+#         grid.setSpacing(10)
+#
+#         grid.addWidget(title, 1, 0)
+#         grid.addWidget(titleEdit, 1, 1)
+#
+#         grid.addWidget(author, 2, 0)
+#         grid.addWidget(authorEdit, 2, 1)
+#
+#         grid.addWidget(review, 3, 0)
+#         grid.addWidget(reviewEdit, 3, 1, 5, 1)
+#
+#         self.setLayout(grid)
+#
+#         self.setGeometry(300, 300, 350, 300)
+#         self.setWindowTitle('Review')
+#         self.show()
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtCore import Qt
+# from PyQt5.QtWidgets import (QWidget, QLCDNumber, QSlider,
+#                              QVBoxLayout, QApplication)
+#
+#
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#         lcd = QLCDNumber(self)
+#         sld = QSlider(Qt.Horizontal, self)
+#
+#         vbox = QVBoxLayout()
+#         vbox.addWidget(lcd)
+#         vbox.addWidget(sld)
+#
+#         self.setLayout(vbox)
+#         sld.valueChanged.connect(lcd.display)
+#
+#         self.setGeometry(300, 300, 250, 150)
+#         self.setWindowTitle('Signal & slot')
+#         self.show()
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtCore import pyqtSignal, QObject
+# from PyQt5.QtWidgets import QMainWindow, QApplication
+#
+#
+# class Communicate(QObject):
+#     closeApp = pyqtSignal()
+#
+#
+# class Example(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#         self.c = Communicate()
+#         self.c.closeApp.connect(self.close)
+#
+#         self.setGeometry(300, 300, 290, 150)
+#         self.setWindowTitle('Emit signal')
+#         self.show()
+#
+#     def mousePressEvent(self, event):
+#         self.c.closeApp.emit()
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit,
+#                              QInputDialog, QApplication)
+#
+#
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#         self.btn = QPushButton('Dialog', self)
+#         self.btn.move(20, 20)
+#         self.btn.clicked.connect(self.showDialog)
+#
+#         self.le = QLineEdit(self)
+#         self.le.move(130, 22)
+#
+#         self.setGeometry(300, 300, 290, 150)
+#         self.setWindowTitle('Input dialog')
+#         self.show()
+#
+#     def showDialog(self):
+#         text, ok = QInputDialog.getText(self, 'Input Dialog',
+#                                         'Enter your name:')
+#
+#         if ok:
+#             self.le.setText(str(text))
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+
+# import sys
+# from PyQt5.QtWidgets import (QWidget, QPushButton, QFrame,
+#                              QColorDialog, QApplication)
+# from PyQt5.QtGui import QColor
+#
+#
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#         col = QColor(0, 0, 0)
+#
+#         self.btn = QPushButton('Dialog', self)
+#         self.btn.move(20, 20)
+#
+#         self.btn.clicked.connect(self.showDialog)
+#
+#         self.frm = QFrame(self)
+#         self.frm.setStyleSheet("QWidget { background-color: %s }"
+#                                % col.name())
+#         self.frm.setGeometry(130, 22, 100, 100)
+#
+#         self.setGeometry(300, 300, 250, 180)
+#         self.setWindowTitle('Color dialog')
+#         self.show()
+#
+#     def showDialog(self):
+#         col = QColorDialog.getColor()
+#
+#         if col.isValid():
+#             self.frm.setStyleSheet("QWidget { background-color: %s }"
+#                                    % col.name())
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QPushButton,
+#                              QSizePolicy, QLabel, QFontDialog, QApplication)
+#
+#
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#         vbox = QVBoxLayout()
+#
+#         btn = QPushButton('Dialog', self)
+#         btn.setSizePolicy(QSizePolicy.Fixed,
+#                           QSizePolicy.Fixed)
+#
+#         btn.move(20, 20)
+#
+#         vbox.addWidget(btn)
+#
+#         btn.clicked.connect(self.showDialog)
+#
+#         self.lbl = QLabel('Knowledge only matters', self)
+#         self.lbl.move(130, 20)
+#
+#         vbox.addWidget(self.lbl)
+#         self.setLayout(vbox)
+#
+#         self.setGeometry(300, 300, 250, 180)
+#         self.setWindowTitle('Font dialog')
+#         self.show()
+#
+#     def showDialog(self):
+#         font, ok = QFontDialog.getFont()
+#         if ok:
+#             self.lbl.setFont(font)
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+
+# import sys
+# from PyQt5.QtWidgets import (QMainWindow, QTextEdit,
+#                              QAction, QFileDialog, QApplication)
+# from PyQt5.QtGui import QIcon
+#
+#
+# class Example(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#         self.textEdit = QTextEdit()
+#         self.setCentralWidget(self.textEdit)
+#         self.statusBar()
+#
+#         openFile = QAction(QIcon('open.png'), 'Open', self)
+#         openFile.setShortcut('Ctrl+O')
+#         openFile.setStatusTip('Open new File')
+#         openFile.triggered.connect(self.showDialog)
+#
+#         menubar = self.menuBar()
+#         fileMenu = menubar.addMenu('&File')
+#         fileMenu.addAction(openFile)
+#
+#         self.setGeometry(300, 300, 350, 300)
+#         self.setWindowTitle('File dialog')
+#         self.show()
+#
+#     def showDialog(self):
+#         fname = QFileDialog.getOpenFileName(self, 'Open file')
+#
+#         if fname[0]:
+#             f = open(fname[0], 'r')
+#
+#             with f:
+#                 data = f.read()
+#                 self.textEdit.setText(data)
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtWidgets import QWidget, QCheckBox, QApplication
+# from PyQt5.QtCore import Qt
+#
+#
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#
+#         cb = QCheckBox('Show title', self)
+#         cb.move(20, 20)
+#         cb.toggle()
+#         cb.stateChanged.connect(self.changeTitle)
+#
+#         self.setGeometry(300, 300, 250, 150)
+#         self.setWindowTitle('QCheckBox')
+#         self.show()
+#
+#     def changeTitle(self, state):
+#
+#         if state == Qt.Checked:
+#             self.setWindowTitle('QCheckBox')
+#         else:
+#             self.setWindowTitle('')
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtWidgets import (QWidget, QPushButton,
+#                              QFrame, QApplication)
+# from PyQt5.QtGui import QColor
+# from PyQt5.QtCore import Qt
+#
+#
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#
+#         self.col = QColor(0, 0, 0)
+#
+#         redb = QPushButton('Red', self)
+#         redb.setCheckable(True)
+#         redb.move(10, 10)
+#
+#         redb.clicked[bool].connect(self.setColor)
+#
+#         greenb = QPushButton('Green', self)
+#         greenb.setCheckable(True)
+#         greenb.move(10, 60)
+#
+#         greenb.clicked[bool].connect(self.setColor)
+#
+#         blueb = QPushButton('Blue', self)
+#         blueb.setCheckable(True)
+#         blueb.move(10, 110)
+#
+#         blueb.clicked[bool].connect(self.setColor)
+#
+#         self.square = QFrame(self)
+#         self.square.setGeometry(150, 20, 100, 100)
+#         self.square.setStyleSheet("QWidget { background-color: %s }" %
+#                                   self.col.name())
+#
+#         self.setGeometry(300, 300, 280, 170)
+#         self.setWindowTitle('Toggle button')
+#         self.show()
+#
+#     def setColor(self, pressed):
+#
+#         source = self.sender()
+#
+#         if pressed:
+#             val = 255
+#         else:
+#             val = 0
+#
+#         if source.text() == "Red":
+#             self.col.setRed(val)
+#         elif source.text() == "Green":
+#             self.col.setGreen(val)
+#         else:
+#             self.col.setBlue(val)
+#
+#         self.square.setStyleSheet("QFrame { background-color: %s }" %
+#                                   self.col.name())
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtWidgets import (QWidget, QProgressBar,
+#                              QPushButton, QApplication)
+# from PyQt5.QtCore import QBasicTimer
+#
+#
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#
+#         self.pbar = QProgressBar(self)
+#         self.pbar.setGeometry(30, 40, 200, 25)
+#
+#         self.btn = QPushButton('Start', self)
+#         self.btn.move(40, 80)
+#         self.btn.clicked.connect(self.doAction)
+#
+#         self.timer = QBasicTimer()
+#         self.step = 0
+#
+#         self.setGeometry(300, 300, 280, 170)
+#         self.setWindowTitle('QProgressBar')
+#         self.show()
+#
+#     def timerEvent(self, e):
+#
+#         if self.step >= 100:
+#             self.timer.stop()
+#             self.btn.setText('Finished')
+#             self.step = 0
+#             return
+#
+#         self.step = self.step + 1
+#         self.pbar.setValue(self.step)
+#
+#     def doAction(self):
+#
+#         if self.timer.isActive():
+#             self.timer.stop()
+#             self.btn.setText('Start')
+#         else:
+#             self.timer.start(100, self)
+#             self.btn.setText('Stop')
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtWidgets import (QWidget, QCalendarWidget,
+#                              QLabel, QApplication)
+# from PyQt5.QtCore import QDate
+#
+#
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#         cal = QCalendarWidget(self)
+#         cal.setGridVisible(True)
+#         cal.move(20, 20)
+#         cal.clicked[QDate].connect(self.showDate)
+#
+#         self.lbl = QLabel(self)
+#         date = cal.selectedDate()
+#         self.lbl.setText(date.toString())
+#         self.lbl.move(130, 260)
+#
+#         self.setGeometry(300, 300, 350, 300)
+#         self.setWindowTitle('Calendar')
+#         self.show()
+#
+#     def showDate(self, date):
+#         self.lbl.setText(date.toString())
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtWidgets import (QWidget, QHBoxLayout,
+#                              QLabel, QApplication)
+# from PyQt5.QtGui import QPixmap
+#
+#
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#         hbox = QHBoxLayout(self)
+#         pixmap = QPixmap("123.jpg")
+#
+#         lbl = QLabel(self)
+#         lbl.setPixmap(pixmap)
+#
+#         hbox.addWidget(lbl)
+#         self.setLayout(hbox)
+#
+#         self.move(300, 200)
+#         self.setWindowTitle('Big tits')
+#         self.show()
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtWidgets import (QWidget, QLabel,
+#                              QLineEdit, QApplication)
+#
+#
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#         self.lbl = QLabel(self)
+#         qle = QLineEdit(self)
+#
+#         qle.move(60, 100)
+#         self.lbl.move(60, 40)
+#
+#         qle.textChanged[str].connect(self.onChanged)
+#
+#         self.setGeometry(300, 300, 280, 170)
+#         self.setWindowTitle('QLineEdit')
+#         self.show()
+#
+#     def onChanged(self, text):
+#         self.lbl.setText(text)
+#         self.lbl.adjustSize()
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+
+import sys
+from PyQt5.QtWidgets import (QWidget, QLabel,
+                             QComboBox, QApplication)
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.lbl = QLabel("Ubuntu", self)
+
+        combo = QComboBox(self)
+        combo.addItem("Ubuntu")
+        combo.addItem("Mandriva")
+        combo.addItem("Fedora")
+        combo.addItem("Arch")
+        combo.addItem("Gentoo")
+
+        combo.move(50, 50)
+        self.lbl.move(50, 150)
+
+        combo.activated[str].connect(self.onActivated)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('QComboBox')
+        self.show()
+
+    def onActivated(self, text):
+        self.lbl.setText(text)
+        self.lbl.adjustSize()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
